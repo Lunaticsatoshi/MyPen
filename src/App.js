4ab8c8836410c0904763795238355a489afc1e19
Lunaticsatoshi <deysarkersudeep@gmail.com>
2020-09-23 21:45:34 +0530

    Made the web app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from './components/Editor';
 
 
 function App() {
 
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [javascript, setJavascript] = useState('')
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [javascript, setJavascript] = useState('');
+  const [srcDoc, setSourceDoc] = useState('')
+
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setSourceDoc(
+        `
+       <html>
+         <body>${html}</body>
+         <style>${css}</style>
+        <script>${javascript}</script>
+       </html>
+  `
+      )
+    }, 250)
+
+    return () => clearTimeout(time)
+  }, [html, css, javascript])
+
 
   return (
     <div className="App">
       <div className="pane top__pane">
-        <Editor 
-        language="xml" 
-        displayName="HTML" 
-        value={html} 
-        onChange={setHtml} />
         <Editor
-        language="css" 
-        displayName="CSS" 
-        value={css} 
-        onChange={setCss} />
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml} />
         <Editor
-        language="javascript" 
-        displayName="Javascript" 
-        value={javascript} 
-        onChange={setJavascript} />
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss} />
+        <Editor
+          language="javascript"
+          displayName="Javascript"
+          value={javascript}
+          onChange={setJavascript} />
       </div>
       <div className="pane">
-        <iframe 
-        title="output"
-        sandbox="allow-scripts"
-        frameBorder="0"
-        width="100%"
-        height="100%"
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
         />
       </div>
     </div>
